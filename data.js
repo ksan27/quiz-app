8e7267be352c0f96500b9c2c1865e92fae5edcd3
ksan27 <73518765+ksan27@users.noreply.github.com>
2021-04-05 18:03:35 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+var quizData = []
+
+var quizData = [{
+  "question": "5 + 5 = ?",
+  "choices": ["10", "55", "5", "0"],
+  "correct": "10"
+},{
+  "question": "10 + 5 = ?",
+  "choices": ["10", "25", "15", "5"],
+  "correct": "15"
+},{
+  "question": "5 + 15 = ?",
+  "choices": ["25", "35", "30", "20"],
+  "correct": "20"
+},{
+  "question": "25 + 5 = ?",
+  "choices": ["30", "55", "35", "40"],
+  "correct": "30"
+}];
